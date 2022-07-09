20ea618d8be0f09aada361f12683a8c835ef8989
郭丽萍 <2019240006@stu.git.edu.cn>
2022-07-09 22:49:16 +0800

    增加头像上传

--- cloudDiskFront/src/pages/person/components/changeInfo/changeInfo.vue
+++ cloudDiskFront/src/pages/person/components/changeInfo/changeInfo.vue
@@ -17,6 +17,7 @@
         <el-button type="primary" @click="confirmChange(1)">确 定</el-button>
       </span>
     </el-dialog>
+
        <el-dialog title="修改个人地址" :visible.sync="show_local" width="30%" :before-close="handleClose">
       <el-input :placeholder="info.userLocal" v-model="input_user_local">
       </el-input>
@@ -25,6 +26,7 @@
         <el-button type="primary" @click="confirmChange(2)">确 定</el-button>
       </span>
     </el-dialog>
+
     <el-dialog title="修改个人描述" :visible.sync="show_introduction" width="30%" :before-close="handleClose">
       <el-input :placeholder="info.userIntroduction" v-model="input_user_introduction">
       </el-input>
@@ -33,6 +35,7 @@
         <el-button type="primary" @click="confirmChange(3)">确 定</el-button>
       </span>
     </el-dialog>
+
       <el-dialog title="管理标签" :visible.sync="show_initialize" width="30%" :before-close="handleClose">
       <!-- <el-input :placeholder="info.userIntroduction" v-model="input_user_name">
       </el-input> -->
@@ -41,6 +44,7 @@
         <el-button type="primary" @click="confirmChange(4)">确 定</el-button>
       </span>
     </el-dialog>
+
     <el-dialog title="修改个人密码" :visible.sync="show_pwd" width="30%" :before-close="handleClose">
       <el-input :placeholder="info.userPwd" v-model="input_user_pwd">
       </el-input>
@@ -49,7 +53,33 @@
         <el-button type="primary" @click="confirmChange(5)">确 定</el-button>
       </span>
     </el-dialog>
-    <el-avatar src="https://pic2.zhimg.com/v2-6a98838ca01408a817ad4b204a5141bb_r.jpg?source=1940ef5c" :size="100">user</el-avatar>
+
+<!--    //用户头像-->
+    <el-upload
+      class="avatar-uploader"
+      :action= action
+      :headers="myHeaders"
+      :show-file-list="false"
+      :on-success="handleAvatarSuccess"
+      :before-upload="beforeAvatarUpload"
+    >
+      <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+      <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+    </el-upload>
+
+<!--&lt;!&ndash;用户头像二    &ndash;&gt;-->
+<!--    <el-upload-->
+<!--      class="avatar-uploader2"-->
+<!--      :action="/api/toUpdateAvatar?userid=1"-->
+<!--      :show-file-list="false"-->
+<!--      accept=".jpg,.png"-->
+<!--      :on-success="handleAvatarSuccess"-->
+<!--      :before-upload="beforeAvatarUpload">-->
+<!--      <img v-if="imageUrl" :src="imageUrl" class="avatar2">-->
+<!--      <i v-else class="el-icon-plus avatar-uploader2-icon"></i>-->
+<!--    </el-upload>-->
+
+<!--    <el-avatar src="https://pic2.zhimg.com/v2-6a98838ca01408a817ad4b204a5141bb_r.jpg?source=1940ef5c" :size="100">user</el-avatar>-->
     <div class="labels">
       <div class="label_left">
         手机号:
